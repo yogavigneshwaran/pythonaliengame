c51a0049d7c69409d78bbeb695b268bc46450674
yogavigneshwaran <yogavigneshwaran@users.noreply.github.com>
2017-11-02 22:39:17 +0530

    Initialized a game project.

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import pygame
+
+from settings import Setting
+from ship import Ship
+import game_functions as gf
+
+class AlienInvasion():
+    def run_game(self):
+        pygame.init()
+        setting = Setting()
+        screen = pygame.display.set_mode((setting.screen_width, setting.screen_height))
+        pygame.display.set_caption('Alien invasion')
+        ship = Ship(screen)
+
+        while True:
+            gf.check_events(ship)
+            gf.update_screen(setting, screen, ship)
+
+
+
+
+
+obj = AlienInvasion()
+obj.run_game()
